8c4a1ac439038229cfea8a59952e2b5f8b94694f
bunyaminmrcn <bunyaminmrcn@gmail.com>
2024-12-03 22:51:56 +0300

    add dependency injection with ninject

--- Northwind.DesktopUI/Form1.cs
+++ Northwind.DesktopUI/Form1.cs
@@ -1,5 +1,6 @@
 using Northwind.Business.Abstract;
 using Northwind.Business.Concrete;
+using Northwind.Business.DependencyResolvers.Ninject;
 using Northwind.DataAccess.Concrete.EntityFramework;
 using Northwind.DataAccess.Concrete.NHibernate;
 using Northwind.Entities.Concrete;
@@ -11,8 +12,8 @@ namespace Northwind.DesktopUI
         public Form1()
         {
             InitializeComponent();
-            _productService = new ProductManager(new EfProductDal());
-            _categoryService = new CategoryManager(new EfCategoryDal());
+            _productService = InstanceFactory.GetInstance<IProductService>();
+            _categoryService = InstanceFactory.GetInstance<ICategoryService>();
         }
 
         private IProductService _productService;
